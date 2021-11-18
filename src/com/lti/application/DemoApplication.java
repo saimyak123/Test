b42b66924f75b3bc264b96c1c907b96a67553fba
SaiChaitanya <mschaitanya98@gmail.com>
2021-11-18 15:14:18 +0530

    Update DemoApplication.java

--- src/com/lti/application/DemoApplication.java
+++ src/com/lti/application/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my first application");
+		System.out.println("my second commit");
 
 	}
 
